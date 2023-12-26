5fd0d62f78428ceb84e79535c7c442bbab46609f
Alexei Kolhan <ak@centaurea.io>
2023-12-27 00:08:02 +0300

    add dbcontext

---
+++ StoreProject/StoreDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StoreProject.Models;
+
+namespace StoreProject;
+
+public class StoreDbContext : IdentityDbContext<User>
+{
+    protected readonly IConfiguration Configuration;
+
+    public StoreDbContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        string? connectionString = Configuration.GetConnectionString("WebApiDatabase");
+        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    }
+    
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+}
